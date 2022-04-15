10de7ef823ee05adc44b1bc3ebb1e76a1f2e8d01
Ayachi <ayachisharma31@gmail.com>
2022-04-15 13:05:40 +0530

    updated forloops with new algos

--- forLoops.c
+++ forLoops.c
@@ -4,6 +4,8 @@ int main() {
     for (int i = 1; i <= 10; i++){
         printf("%d \n", i);
     }
+    printf(" done with for loops \n");
+    printf("wait what about while loops ? \n");
 
     return 0;
 }
